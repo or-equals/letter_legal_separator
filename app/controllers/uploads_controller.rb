e87b6ef8b0f7704efca513a7d51f73e72a6e1607
Joshua Plicque <plicjo@gmail.com>
2020-09-20 01:30:30 -0400

    Remove unused code paths

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,15 +1,8 @@
 class UploadsController < ApplicationController
-  before_action :set_upload, only: [:show]
-
-  def new
-    @upload = Upload.new
-  end
-
   def create
     @upload = Upload.new(upload_params)
 
     if @upload.save
-      flash[:notice] = "Upload was successfully created."
       send_data @upload.pdf_file.download, filename: @upload.pdf_file.filename.to_s, content_type: @upload.pdf_file.content_type
     else
       render 'pages/index'
@@ -17,10 +10,6 @@ class UploadsController < ApplicationController
   end
 
   private
-
-  def set_upload
-    @upload = Upload.find(params[:id])
-  end
 
   def upload_params
     params.require(:upload).permit(:pdf_file)
