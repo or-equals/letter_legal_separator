44da036c0c2cb83ba564cc8f85eddbfc4b9f7947
Joshua Plicque <plicjo@gmail.com>
2020-12-20 22:22:11 -0500

    Update copy

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,6 +1,5 @@
 class AdminMailer < ApplicationMailer
-
   def pdf_uploaded
-    mail(to: 'josh@ghed.dev', subject: 'Somebody generated a document')
+    mail(to: 'josh@orequals.com', subject: 'Somebody generated a document')
   end
 end
