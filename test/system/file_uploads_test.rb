36d0df1b64478ae191f3fb6fd83fd85aa06fca26
Joshua Plicque <plicjo@gmail.com>
2020-12-20 21:33:52 -0500

    Remove test folder

--- test/system/file_uploads_test.rb
+++
@@ -1,8 +0,0 @@
-require "application_system_test_case"
-
-class FileUploadsTest < ApplicationSystemTestCase
-   test "visiting the index" do
-     visit root_path
-     assert_selector "h2", text: "Letter Legal Page Separator"
-   end
-end
